1678f48dde361380cfd8b895852355739f559e92
nin-jin <nin-jin@users.noreply.github.com>
2020-06-01 07:03:06 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.esm.js
+++ web.esm.js
@@ -5317,6 +5317,12 @@ var $;
             version() {
                 return this.stat().mtime.getTime().toString(36).toUpperCase();
             }
+            watcher() {
+                console.warn('$mol_file_web.watcher() not implemented');
+                return {
+                    destructor() { }
+                };
+            }
             exists(next, force) {
                 let exists = true;
                 try {
@@ -5582,9 +5588,6 @@ var $;
                 if (response.native.status === 404)
                     throw new $.$mol_file_not_found(`File not found: ${this.path()}`);
                 return new Uint8Array(response.buffer());
-            }
-            watcher() {
-                throw new Error('$mol_file_web.watcher() not implemented');
             }
             stat(next, force) {
                 let stat = next;
