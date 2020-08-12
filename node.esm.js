82409e23de828ba02792f233e990f1fef142bba2
nin-jin <nin-jin@users.noreply.github.com>
2020-08-12 07:04:59 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -4156,6 +4156,12 @@ var $;
             }
             return found;
         }
+        size() {
+            switch (this.type()) {
+                case 'file': return this.stat().size;
+                default: return 0;
+            }
+        }
     }
     __decorate([
         $.$mol_mem
