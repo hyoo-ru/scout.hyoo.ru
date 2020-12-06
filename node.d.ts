9887ba1c385e6ed547122123320644081fd780a8
nin-jin <nin-jin@users.noreply.github.com>
2020-12-06 07:04:26 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -1289,7 +1289,6 @@ declare namespace $ {
             target: string;
             download: string;
             mol_link_current: boolean;
-            mol_theme: any;
         };
         sub(): readonly (string | number | boolean | $mol_view | Node)[];
         arg(): {};
@@ -1301,7 +1300,6 @@ declare namespace $ {
         target(): string;
         file_name(): string;
         current(): boolean;
-        theme(): any;
         event_click(event?: any): any;
         click(event?: any): any;
     }
@@ -1340,7 +1338,6 @@ declare namespace $.$$ {
         event_click(event?: Event): void;
         file_name(): string;
         minimal_height(): number;
-        theme(): "$mol_theme_base" | null;
     }
 }
 
@@ -2351,7 +2348,6 @@ declare namespace $ {
             target: string;
             download: string;
             mol_link_current: boolean;
-            mol_theme: any;
         };
         uri(): any;
         content(val?: any): any;
@@ -2551,7 +2547,7 @@ declare namespace $.$$ {
 
 declare namespace $ {
     class $mol_switch extends $mol_view {
-        Option(id: any): $$.$mol_switch_option;
+        Option(id: any): $$.$mol_check;
         value(val?: any): any;
         options(): {};
         keys(): readonly string[];
@@ -2562,19 +2558,6 @@ declare namespace $ {
         enabled(): boolean;
         option_enabled(id: any): boolean;
         items(): readonly $mol_check[];
-    }
-    class $mol_switch_option extends $mol_check {
-        minimal_height(): number;
-        attr(): {
-            mol_theme: string;
-            mol_check_checked: any;
-            "aria-checked": any;
-            role: string;
-            disabled: boolean;
-            tabindex: number;
-            title: string;
-        };
-        theme(): string;
     }
 }
 
@@ -2588,12 +2571,9 @@ declare namespace $.$$ {
             [key: string]: string;
         };
         keys(): string[];
-        items(): $mol_switch_option[];
+        items(): $mol_check[];
         option_title(key: string): string;
         option_checked(key: string, next?: boolean): boolean | undefined;
-    }
-    class $mol_switch_option extends $.$mol_switch_option {
-        theme(): "" | "$mol_theme_base";
     }
 }
 
