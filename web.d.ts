7caedb7a2ff427deb173d2ae769b39328b67ee2a
nin-jin <nin-jin@users.noreply.github.com>
2021-01-24 07:46:42 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -2496,6 +2496,16 @@ declare namespace $ {
         };
         type(): string;
     }
+    class $mol_text_code_token_link extends $mol_text_code_token {
+        dom_name(): string;
+        type(): string;
+        attr(): {
+            href: string;
+            target: string;
+            mol_text_code_token_type: string;
+        };
+        haystack(): string;
+    }
 }
 
 declare namespace $.$$ {
@@ -2506,6 +2516,7 @@ declare namespace $ {
         text(): string;
         minimal_height(): number;
         Token(id: any): $mol_text_code_token;
+        Token_link(id: any): $mol_text_code_token_link;
         token_type(id: any): string;
         token_text(id: any): string;
         highlight(): string;
@@ -2522,6 +2533,7 @@ declare namespace $.$$ {
         }[];
         sub(): $mol_text_code_token[];
         row_content(path: number[]): $mol_text_code_token[];
+        Token(path: number[]): $mol_text_code_token;
         token_type(path: number[]): string;
         token_content(path: number[]): (string | $mol_text_code_token)[];
         token_text(path: number[]): string;
