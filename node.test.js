654e83a667175528729e10e3e7d16fbc88f3d823
nin-jin <nin-jin@users.noreply.github.com>
2022-07-30 07:08:54 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -3430,6 +3430,7 @@ var $;
             return {
                 ...super.event(),
                 click: (event) => this.event_activate(event),
+                dblclick: (event) => this.clicks(event),
                 keydown: (event) => this.event_key_press(event)
             };
         }
@@ -3453,6 +3454,11 @@ var $;
             return obj;
         }
         event_activate(event) {
+            if (event !== undefined)
+                return event;
+            return null;
+        }
+        clicks(event) {
             if (event !== undefined)
                 return event;
             return null;
@@ -3487,6 +3493,9 @@ var $;
     __decorate([
         $mol_mem
     ], $mol_button.prototype, "event_activate", null);
+    __decorate([
+        $mol_mem
+    ], $mol_button.prototype, "clicks", null);
     __decorate([
         $mol_mem
     ], $mol_button.prototype, "event_key_press", null);
