c9cc65bd7a3bac6260a8b123849757488c8e112a
nin-jin <nin-jin@users.noreply.github.com>
2022-09-17 07:10:46 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -5645,7 +5645,10 @@ var $;
         selection(val) {
             if (val !== undefined)
                 return val;
-            return [];
+            return [
+                0,
+                0
+            ];
         }
         auto() {
             return [
@@ -5661,7 +5664,8 @@ var $;
                 spellcheck: this.spellcheck(),
                 autocomplete: this.autocomplete_native(),
                 selectionEnd: this.selection_end(),
-                selectionStart: this.selection_start()
+                selectionStart: this.selection_start(),
+                inputMode: this.keyboard()
             };
         }
         attr() {
@@ -5714,6 +5718,9 @@ var $;
         }
         selection_start() {
             return 0;
+        }
+        keyboard() {
+            return "text";
         }
         length_max() {
             return Infinity;
@@ -5787,7 +5794,7 @@ var $;
             event_change(next) {
                 if (!next)
                     return;
-                this.value(next.target.value);
+                this.value_changed(next.target.value);
                 this.selection_change(next);
             }
             hint_visible() {
@@ -5806,10 +5813,12 @@ var $;
                 const el = this.dom_node();
                 if (el !== this.$.$mol_dom_context.document.activeElement)
                     return;
-                this.selection([
+                const [from, to] = this.selection([
                     el.selectionStart,
                     el.selectionEnd,
                 ]);
+                el.selectionEnd = to;
+                el.selectionStart = from;
             }
             selection_start() {
                 return this.selection()[0];
--- web.d.ts
+++ web.d.ts
@@ -1714,6 +1714,7 @@ declare namespace $ {
             autocomplete: string;
             selectionEnd: number;
             selectionStart: number;
+            inputMode: string;
         };
         attr(): {
             maxlength: number;
@@ -1734,6 +1735,7 @@ declare namespace $ {
         autocomplete_native(): string;
         selection_end(): number;
         selection_start(): number;
+        keyboard(): string;
         length_max(): number;
         type(val?: any): string;
         event_change(event?: any): any;
@@ -2235,6 +2237,7 @@ declare namespace $ {
             autocomplete: string;
             selectionEnd: number;
             selectionStart: number;
+            inputMode: string;
         };
     }
 }
