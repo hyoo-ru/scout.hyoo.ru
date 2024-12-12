93aa94614da19b024680537dde91c5c32f5539d6
nin-jin <nin-jin@users.noreply.github.com>
2024-12-12 07:14:22 +0000

    Deploying to gh-pages from @ hyoo-ru/scout.hyoo.ru@3170164602250c5993bc388c1a06ea5c1a2780a5 🚀

--- node.js
+++ node.js
@@ -5649,7 +5649,7 @@ var $;
     var $$;
     (function ($$) {
         const { per, rem } = $mol_style_unit;
-        const { hsla } = $mol_style_func;
+        const { hsla, blur } = $mol_style_func;
         $mol_style_define($mol_page, {
             display: 'flex',
             flex: {
@@ -5662,6 +5662,7 @@ var $;
             maxHeight: per(100),
             boxSizing: 'border-box',
             color: $mol_theme.text,
+            backdropFilter: blur(`3px`),
             ':focus': {
                 outline: 'none',
             },
