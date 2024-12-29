e70b84532652273a4aac709d1bdee854d19a6026
nin-jin <nin-jin@users.noreply.github.com>
2024-12-29 07:12:01 +0000

    Deploying to gh-pages from @ hyoo-ru/scout.hyoo.ru@3170164602250c5993bc388c1a06ea5c1a2780a5 🚀

--- web.js
+++ web.js
@@ -5536,7 +5536,7 @@ var $;
             Tools: {
                 flex: {
                     basis: 'auto',
-                    grow: 1000,
+                    grow: 0,
                     shrink: 1,
                 },
                 display: 'flex',
