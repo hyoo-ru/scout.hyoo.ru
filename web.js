11be86a05f424eedcdf7924601a378fc48aa9dc8
nin-jin <nin-jin@users.noreply.github.com>
2025-09-05 07:14:08 +0000

    Deploying to gh-pages from @ hyoo-ru/scout.hyoo.ru@3170164602250c5993bc388c1a06ea5c1a2780a5 🚀

--- web.js
+++ web.js
@@ -3275,7 +3275,7 @@ var $;
             return node;
         }
         auto() {
-            return null;
+            return [];
         }
         render() {
             const node = this.dom_node_actual();
@@ -11956,6 +11956,7 @@ var $;
                         return response;
                     const headers = new Headers(response.headers);
                     headers.set("$mol_offline", "");
+                    headers.set("Origin-Agent-Cluster", "?1");
                     return new Response(response.body, {
                         status: response.status,
                         statusText: response.statusText,
